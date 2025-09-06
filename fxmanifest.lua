c36fc74ac14f7a4c0216fb78b828ece183a5930c
MaDHouSe <madhouse1979@gmail.com>
2025-09-06 13:07:55 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,18 +7,16 @@ game 'gta5'
 lua54 'yes'
 
 author 'MaDHouSe79'
-description 'MH Cooldown - Use this in other scripts to cooldown any actions or triggers'
+description 'MH Cooldown (Server Side) - Use this in other scripts to cooldown any actions or triggers'
 version '1.0.0'
-
-shared_scripts {
-    '@ox_lib/init.lua',
-    'locales/locale.lua',
-    'locales/en.lua', -- change this to your own lang file.
-}
 
 server_only 'yes'
 
 server_scripts {
+    '@ox_lib/init.lua',
+    'locales/locale.lua',
+    'locales/en.lua', -- change this to your own lang file.
+    --
     'config.lua',
     'server/main.lua',
     'server/update.lua',
